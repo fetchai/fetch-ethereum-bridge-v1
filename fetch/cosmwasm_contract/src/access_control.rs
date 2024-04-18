cf8b882e1ae02438d760a7af9c1a2d71f3b88114
Jiri <jiri.vestfal@fetch.ai>
2024-04-18 17:32:07 +0200

    Removing all roles impl

--- fetch/cosmwasm_contract/src/access_control.rs
+++ fetch/cosmwasm_contract/src/access_control.rs
@@ -1,4 +1,4 @@
-use cosmwasm_std::{Addr, StdError, StdResult, Storage};
+use cosmwasm_std::{Addr, Order, StdError, StdResult, Storage};
 use cosmwasm_storage::{PrefixedStorage, ReadonlyPrefixedStorage};
 use std::str::FromStr;
 
@@ -94,4 +94,72 @@ pub fn ac_revoke_role(
         PrefixedStorage::multilevel(storage, &[ACCESS_CONTROL_KEY, addr.as_bytes()]);
     addr_roles.remove(role.as_bytes());
     Ok(true)
+}
+
+pub fn unsafe_remove_all_roles(storage: &mut dyn Storage) {
+    let mut roles_storage = PrefixedStorage::new(storage, ACCESS_CONTROL_KEY);
+    let res: Vec<_> = roles_storage.range(None, None, Order::Ascending).collect();
+
+    // Because address is stored as first key it is impossible to clear only specific roles without manually filtering it
+    for val in res {
+        roles_storage.remove(&val.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use cosmwasm_std::testing::mock_dependencies;
+
+    #[test]
+    fn test_unsafe_remove_all_roles() {
+        let mut deps = mock_dependencies();
+
+        let addresses = vec!["a", "b", "c", "d"];
+        let roles = vec![
+            AccessRole::Admin,
+            AccessRole::Approver,
+            AccessRole::Monitor,
+            AccessRole::Relayer,
+        ];
+
+        // Add every role and address combination
+        for address in &addresses {
+            for role in &roles {
+                ac_add_role(
+                    deps.as_mut().storage,
+                    &Addr::unchecked(address.to_string()),
+                    role,
+                )
+                .unwrap();
+            }
+        }
+
+        // Every role and address combination is present
+        for address in &addresses {
+            for role in &roles {
+                assert!(ac_have_role(
+                    deps.as_ref().storage,
+                    &Addr::unchecked(address.to_string()),
+                    role
+                )
+                .unwrap());
+            }
+        }
+
+        // Remove all roles
+        unsafe_remove_all_roles(deps.as_mut().storage);
+
+        // Every role and address combination was removed
+        for address in &addresses {
+            for role in &roles {
+                assert!(!ac_have_role(
+                    deps.as_ref().storage,
+                    &Addr::unchecked(address.to_string()),
+                    role
+                )
+                .unwrap());
+            }
+        }
+    }
 }
